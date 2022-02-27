edf3d6bd433561e099fdc6ac6ca52d2478966fd4
dggjsc <72360401+dggjsc@users.noreply.github.com>
2022-02-27 13:37:06 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/time')
 def hello_world():
     from time import gmtime, strftime
     return strftime("%Y-%m-%d %H:%M:%S", gmtime())
